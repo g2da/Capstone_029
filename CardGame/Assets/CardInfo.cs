1bf3b861107c868a7d0f28ed2d29ab4b97bcb1a9
wxxhyeong <wxxhyeong@gmail.com>
2022-02-16 01:15:03 +0900

    game

---
+++ CardGame/Assets/CardInfo.cs
@@ -0,0 +1,26 @@
+﻿//카드 정보를 담을 클래스 스크립트
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class CardInfo : MonoBehaviour
+{
+    public string cardName;
+    public int cardStatus =0;
+    public Sprite wordImage;
+
+    public CardInfo()
+    {
+        
+    }
+
+    public CardInfo(string CardName,Sprite WordImage)
+    {
+        cardName = CardName;
+        wordImage = WordImage;
+
+    }
+}
+
---
+++ CardGame/Assets/rotation1.cs
@@ -0,0 +1,85 @@
+﻿//카드 회전 담당 스크립트
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class rotation1 : MonoBehaviour
+{
+    private SpriteRenderer rend; //이미지를 표현하는 속성을 가진 객체
+    CardInfo thisCard;
+
+    [SerializeField] //private 변수를 inspector에서 접근가능하게 해줌
+    private Sprite faceSprite, backSprite;
+
+    private bool coroutineAllowed, facedUp;
+
+    void Start()
+    {
+        gameObject.AddComponent<CardInfo>();
+        rend = GetComponent<SpriteRenderer>();
+        thisCard = GetComponent<CardInfo>();
+
+        rend.sprite = backSprite;
+        coroutineAllowed = true;
+        facedUp = false;
+        
+
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine(RotateCard());
+        }
+    }
+
+
+    private IEnumerator RotateCard()
+    {
+        coroutineAllowed = false;
+        if (!facedUp)
+        {
+            for (float i = 0f; i <= 180f; i += 10f)
+            {
+                transform.rotation = Quaternion.Euler(0f, i, 0f);
+                if (i == 90f)
+                {
+                    rend.sprite = faceSprite;
+                    thisCard.cardStatus = 1;
+
+                    GameObject director = GameObject.Find("GameDirector");
+                    director.GetComponent<GameDirector>().Flip_Count();
+                    
+
+                }
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+
+        else if (facedUp)
+        {
+            for (float i = 180f; i >= 0f; i -= 10f)
+            {
+                transform.rotation = Quaternion.Euler(0f, i, 0f);
+                if (i == 90f)
+                {
+                    rend.sprite = backSprite;
+                    thisCard.cardStatus = 0;
+                    
+                }
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+        coroutineAllowed = true;
+
+        facedUp = !facedUp;
+    }
+
+    // Update is called once per frame
+
+}
